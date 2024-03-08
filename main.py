c36bbeb284f82e73796eaa29a4589b352c86521c
GohJianLin <160487674+GohJianLin@users.noreply.github.com>
2024-03-08 15:57:10 +0000

    Final submission

--- main.py
+++ main.py
@@ -2,3 +2,13 @@ stdform = input('Enter a number in scientific notation: ')
 stdform = stdform.strip()
 
 # Type your code below
+valid = ["1","2","3",'4','5','6','7','8','9','0',"E","x",".","-","^"]
+k = stdform.find("^") 
+digit = 0
+for i in range(len(stdform)):
+  if str(stdform)[i] not in valid:
+    digit += 1
+if digit == 0 and stdform.count(".") == 1 and stdform[k+1:].isdigit:
+  print(f"This number in E notation is {stdform.replace('x10^','E')}.")
+else: 
+  print("Error converting to scientific E notation.")
